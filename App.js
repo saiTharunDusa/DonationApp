fad1fafa0cfef6c24f264b58a3608f6ab34f1c69
Sai Tharun Dusa <sd2672@nau.edu>
2025-03-19 16:26:41 -0700

    Initial SetUp

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+const App = () =>  {
+  return(
+    <SafeAreaView>
+      <Text style={{
+        fontSize:70,
+        fontFamily : 'Inter'
+      }}>Hello World!</Text>
+    </SafeAreaView>
+  )
+};
+
+export default App;
